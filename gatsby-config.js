2aaeaffa812a7aa541df42bdfb23bb50c401bb33
WPMUDEV SLS Snippets - Unofficial <30955326+wpmudev-sls@users.noreply.github.com>
2023-02-06 15:18:49 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   plugins: ["@chakra-ui/gatsby-plugin"],
-  pathPrefix: "sls-gists",
+  pathPrefix: "/gh-site",
   siteMetadata: {
     title: `Zip Github Files`,
     siteUrl: `https://github.com/wpmudev/sls-gists`,
